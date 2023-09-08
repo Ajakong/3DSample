ae9754299b64cba32140fff9dfb30a81b4d526de
Ajakong <135081816+Ajakong@users.noreply.github.com>
2023-09-08 17:19:06 +0900

    up

--- 3DSample/Assets/playerMove.cs
+++ 3DSample/Assets/playerMove.cs
@@ -1,26 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
     Rigidbody myRb;
 
+    int rolltimer=0;
     float rotate=0;
+    bool rollFlag=false;
     Vector3 move;
     Vector3 angle;
+
+    Vector3 x,y,z;
+    Quaternion rolling;
     // Start is called before the first frame update
     void Start()
     {
        
         angle = new Vector3(0, 0.1f, 0);
         myRb = this.GetComponent<Rigidbody>();
+
+        x=new Vector3 (0.1f,0,0);
+        rolling=new Quaternion(0.01f, 0, 0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
         move = transform.forward * 0.02f;
+        
         if (Input.GetKey(KeyCode.W))
         {
             
@@ -47,5 +57,29 @@ public class playerMove : MonoBehaviour
            
             this.transform.rotation = Quaternion.AngleAxis(rotate, angle);
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rollFlag = true;
+
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (rollFlag == true)
+        {
+            if (rolltimer < 10)
+            {
+                this.transform.rotation = Quaternion.AngleAxis(rotate, angle);
+                myRb.position += move * 30;
+                rolltimer++;
+            }
+            else
+            {
+                rolltimer = 0;
+                rollFlag = false;
+            }
+        }
+
+
     }
 }
